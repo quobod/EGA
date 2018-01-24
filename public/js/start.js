3f47c019a0e29113661941ccf180b7e91c7f2da7
quobod <quobod@gmail.com>
2018-01-24 05:57:59 -0500

    Installed csurf module

--- public/js/start.js
+++ public/js/start.js
@@ -1,74 +1,75 @@
 window.onload = () => {initApp();};
 
 const initApp = () => {
-    const urls = [
-        {name: cfc('posts'), url: posts},
-        {name: cfc('post'), url: post},
-        {name: cfc('photos'), url: photos},
-        {name: cfc('comments'), url: comments},
-        {name: cfc('users'), url: users},
-        {name: cfc('jokes'), url: jokes},
-        {name: cfc('albums'), url: albums}
-    ];
-    const count = urls.length;
-    const output = getElement('output');
-    
-    // Clear output div
-    removeElements(output);
-
-    urls.map(url => {
-        // Create the parent div
-        let parent = createElement('div');
-
-        // Add to the output div
-        appendElement(output, parent);
+    if (document.title.toLowerCase() == 'ega project') {
+        const urls = [
+            {name: cfc('posts'), url: posts},
+            {name: cfc('post'), url: post},
+            {name: cfc('photos'), url: photos},
+            {name: cfc('comments'), url: comments},
+            {name: cfc('users'), url: users},
+            {name: cfc('jokes'), url: jokes},
+            {name: cfc('albums'), url: albums}
+        ];
+        const count = urls.length;
+        const output = getElement('output');
         
-        // Add proper attributes
-        addAttribute(parent, 'class', 'row');
+        // Clear output div
+        removeElements(output);
 
-        // Create the header
-        let header = createElement('div');
-        
-        // Add proper attributes
-        addAttribute(header, 'class', 'header');
-        
-        // Add a header element to header
-        let h = createElement('h3');
-        h.innerHTML = `${url.name}`;
+        urls.map(url => {
+            // Create the parent div
+            let parent = createElement('div');
 
-        // Add it to header
-        appendElement(header, h);
+            // Add to the output div
+            appendElement(output, parent);
+            
+            // Add proper attributes
+            addAttribute(parent, 'class', 'row');
 
-        // Add to the parent
-        appendElement(parent, header);
+            // Create the header
+            let header = createElement('div');
+            
+            // Add proper attributes
+            addAttribute(header, 'class', 'header');
+            
+            // Add a header element to header
+            let h = createElement('h3');
+            h.innerHTML = `${url.name}`;
 
-        // Create the main body
-        let main = createElement('div');
-        addAttribute(main, 'class', 'body');
+            // Add it to header
+            appendElement(header, h);
 
-        // Create the body's inners
-        main.innerHTML = `<a href="${url.url}" target="_blank">Get ${url.name}</a>`;
+            // Add to the parent
+            appendElement(parent, header);
 
-        // Add to the parent
-        appendElement(parent, main);
+            // Create the main body
+            let main = createElement('div');
+            addAttribute(main, 'class', 'body');
 
-        // Create the footer
-        let footer = createElement('div');
+            // Create the body's inners
+            main.innerHTML = `<a href="${url.url}" target="_blank">Get ${url.name}</a>`;
 
-        // Add a para element to footer
-        let p = createElement('p');
-        addAttribute(p, 'class', 'v-scroll');
-        p.innerHTML = `${url.url} <span class="copyright">Queueb &copy;2018</span>`;
+            // Add to the parent
+            appendElement(parent, main);
 
-        // Add para to the footer
-        appendElement(footer, p);
-                
-        // Add proper attributes
-        addAttribute(footer, 'class', 'footer');
-       
-        // Add to the parent
-        appendElement(parent, footer);
+            // Create the footer
+            let footer = createElement('div');
 
-    });
+            // Add a para element to footer
+            let p = createElement('p');
+            addAttribute(p, 'class', 'v-scroll');
+            p.innerHTML = `${url.url} <span class="copyright">Queueb &copy;2018</span>`;
 
+            // Add para to the footer
+            appendElement(footer, p);
+                    
+            // Add proper attributes
+            addAttribute(footer, 'class', 'footer');
+           
+            // Add to the parent
+            appendElement(parent, footer);
+
+        });
+    }
 }; 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,28 @@ const express = require('express');
 const router = express.Router();
 const log = require('../custom_modules/log');
 
+// Home view
 router.get('/', (req, res) => {
     
-    res.render('index', {title:'EGA Project', korn: 'I seen it all'});
+    res.render('index', {title:'EGA Project'});
+});
+
+// About view
+router.get('/about', (req, res) => {
+    
+    res.render('about', {title:'About Us'});
+});
+
+// Contact view
+router.get('/contact', (req, res) => {
+    
+    res.render('contact', {title:'Contact Us'});
+});
+
+// Search view
+router.get('/search', (req, res) => {
+    
+    res.render('search', {title:'Search'});
 });
 
 module.exports = router;
