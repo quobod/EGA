48769f292631d54d44b18bcc4dd40ad9456d2803
quobod <quobod@gmail.com>
2018-01-27 10:52:47 -0500

    Added custom modules and unit test

--- custom_modules/struts.js
+++ custom_modules/struts.js
@@ -1,11 +1,75 @@
 const uuid = require('uuid');
 const chalk = require('chalk');
 
+const dash = (arg) => {
+    return arg.match('-');
+};
+
+const space = (arg) => {
+    return arg.match(' ');
+};
+
+const underscore = (arg) => {
+    return arg.match('_');
+};
+
+const validArg = (arg) => {
+    return null !== arg && undefined !== arg && (typeof arg === 'string' && arg.length > 0);
+};
+
+const capitalizeFirstCharacter = (arg, character = null) => {
+    if (null === character) {
+        return arg.substring(0, 1).toUpperCase();
+    } else {
+        let newWord = '';
+        const argSplit = arg.split(character);
+        for (let i = 0; i < argSplit.length; i++) {
+            let line = argSplit[i];
+            if (i < (argSplit.length - 1)) {
+                newWord += line.substring(0, 1).toUpperCase() + line.substring(1) + ' ';
+            } else {
+                newWord += line.substring(0, 1).toUpperCase() + line.substring(1);
+            }
+        }
+        return newWord;
+    }
+};
+
+const cfc = (arg) => {
+    if (validArg(arg)) {
+        if (null !== dash(arg)) {
+            return capitalizeFirstCharacter(arg, '-');
+        } else if (null !== space(arg)) {
+            return capitalizeFirstCharacter(arg, ' ');
+        } else if (null !== underscore(arg)) {
+            return capitalizeFirstCharacter(arg, '_');
+        } else {
+            return capitalizeFirstCharacter(arg);
+        }
+    } else {
+        return null;
+    }
+};
 
 module.exports = {
+    'dash': (function(){
+        return function(arg) {
+            return dash(arg);
+        }
+    })(),
+    'space': (function(){
+        return function(arg) {
+            return space(arg);
+        }
+    })(),
+    'underscore': (function(){
+        return function(arg) {
+            return underscore(arg);
+        }
+    })(),
     'cfc': (function(){
-        return function(word) {
-            return cfc(word);
+        return function(arg) {
+            return cfc(arg);
         }
     })()
 }
---
+++ test/struts-tests.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+const chalk = require('chalk');
+const log = require('../custom_modules/log');
+const struts = require('../custom_modules/struts');
+	
+log('\n\t\tModule Struts Test Cases\n');
+
+// Testing the dash function
+describe(chalk.green('Dash function test'), () => {
+    describe(chalk.yellow('Testing 5 strings for the dash character'), () => {
+        it('should return 2 positives and 3 negative matches', () => {
+            const args = ['to-prevent-this', 'in_prepared_testimony_in', 'with-both_your-counter-parts', 'are there no go zones', 'surely_I_saw'];
+            assert.notEqual(null, struts.dash(args[0]));
+            assert.equal(null, struts.dash(args[1]));
+            assert.notEqual(null, struts.dash(args[2]));
+            assert.equal(null, struts.dash(args[3]));
+            assert.equal(null, struts.dash(args[4]));
+        });
+    });
+});
+
+// Testing the single space function
+describe(chalk.green('Space function test'), () => {
+    describe(chalk.yellow('Testing 5 strings for any single space characters'), () => {
+        it('should return 1 positive and 4 negatives matches', () => {
+            const args = ['to-prevent-this', 'in_prepared_testimony_in', 'with-both_your-counter-parts', 'are there no_go-zones, has not', 'surely_I_saw'];
+            assert.equal(null, struts.space(args[0]));
+            assert.equal(null, struts.space(args[1]));
+            assert.equal(null, struts.space(args[2]));
+            assert.notEqual(null, struts.space(args[3]));
+            assert.equal(null, struts.space(args[4]));
+        });
+    });
+});
+
+// Testing the underscore function
+describe(chalk.green('Underscore function test'), () => {
+    describe(chalk.yellow('Testing 5 strings for any underscore characters'), () => {
+        it('should return 4 positives and 1 negative matches', () => {
+            const args = ['to-prevent-this', 'in_prepared_testimony_in', 'with-both_your-counter-parts', 'are there no_go-zones, has not', 'surely_I_saw'];
+            assert.equal(null, struts.underscore(args[0]));
+            assert.notEqual(null, struts.underscore(args[1]));
+            assert.notEqual(null, struts.underscore(args[2]));
+            assert.notEqual(null, struts.underscore(args[3]));
+            assert.notEqual(null, struts.underscore(args[4]));
+        });
+    });
+});
+
+// Testing the cfc function
+describe(chalk.green('Underscore function test'), () => {
+    describe(chalk.yellow('Testing 5 strings for with underscore, single space and dashes'), () => {
+        it('should return all strings with first character of each word in caps', () => {
+            const args = ['to-prevent-this', 'in_prepared_testimony_in', 'with-both-your-counter-parts', 'are_there_no_go_zones', 'has not', 'surely_I_saw'];
+            assert.notEqual(null, struts.cfc(args[0]));
+            assert.notEqual(null, struts.cfc(args[1]));
+            assert.notEqual(null, struts.cfc(args[2]));
+            assert.notEqual(null, struts.cfc(args[3]));
+            assert.notEqual(null, struts.cfc(args[4]));
+            args.map(arg => {
+                log(struts.cfc(arg));
+            });
+        });
+    });
+});
