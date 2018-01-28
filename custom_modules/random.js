f7efdab0837ee24b0667b367dd2d526fa2d51328
quobod <quobod@gmail.com>
2018-01-27 19:09:45 -0500

    Added custom module

---
+++ custom_modules/random.js
@@ -0,0 +1,35 @@
+    /*  Returns a random member. Expecting an Array
+     *  @param arg An array
+     *  @return Random member*/
+    const randomMember = (arg) => {
+        return arg[~~(Math.random() * (arg.length-1))];
+    }
+
+    /*  Returns a random number within given range. Expecting two numbers
+     *  @param min Number or String number
+     *  @param max Number or String number
+     *  @return A random number between the minimum and maximum range*/
+    const randomBetween = (min, max) => {    
+        if (typeof min === 'string') {
+            min = parseInt(min);
+        }
+
+        if (typeof max === 'string') {
+            max = parseInt(max);
+        }
+
+        return ~~(Math.random() * (max-min)) + min;
+    }
+
+module.exports = {
+    'member': (function(){
+        return function(arg) {
+            return randomMember(arg);
+        }
+    })(),
+    'between': (function(){
+        return function(arg1, arg2) {
+            return randomBetween(arg1, arg2);
+        }
+    })()
+};
